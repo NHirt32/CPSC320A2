1d61c8c01940897b4050034728b8c2d59c8776e9
NHirt32 <nicholas.t.hirt@gmail.com>
2023-02-02 22:03:21 -0800

    I hate this recursive descent stuff, really not sure how to handle it. Question 8 finished.

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import re
 
 class lexer():
     def Tokenize(input):
-        colinRegex = re.compile("([a-z][a-z]+|\d+|\<|\+|-)")
+        colinRegex = re.compile("([a-z][a-z]+|\d+|\<|\+|-)") #Super regex used for initial list creation
 
         identifier = re.compile("[a-z][a-z]+")  # Regex for detecting identifiers
         integer = re.compile("\d")  # Regex for detecting digits
@@ -37,21 +37,75 @@ class parseTree():
     def __int__(self, root):
         self.root = Node(root)  # Defining our root value in our node
 
+
 class descent():
 
-    def Parse(input):
+    def progParse(self, inputList):
+            index = -1;
+            match self.lookAhead(inputList, index):
+                case "<integer>>":
+                    index += 1
+                    self.integerParse()
+                    return
+                case "<operator>":
+                    index += 1
+                    self.operatorParse()
+                case _:
+                    return
+
+        return
+
+    def save_varParse(self, inputList, index):
+        return
+
+    def exprParse(self, inputList, index):
+        return
+
+    def literalParse(self, inputList, index):
+        return
+
+    def integerParse(self, intTuple):
+        if intTuple[1] == "<integer>":          #If we are dealing with an integer
+            intTree = parseTree("<integer>")    #Create a node with the <integer> value
+            intTree.addNode(intTuple[0])        #Create a child node with whatever the actual value is from the input tuple
+            return intTree
+        return False    #The input tuple did not match our method
+
+    def identifierParse(self, identifierTuple):
+        if identifierTuple[1] == "<identifier>":              #If we are dealing with an identifier
+            identifierTree = parseTree("<identifier>")        #Create a node with the <identifier> value
+            identifierTree.addNode(identifierTuple[0])        #Create a child node with whatever the actual value is from the input tuple
+            return identifierTree
+        return False    #The input tuple did not match our method
+
+    def operatorParse(self, operatorTuple):
+        if operatorTuple[1] == "<operator>":        # If we are dealing with an operator
+            operatorTree = parseTree("<operator>")  # Create a node with the <operator> value
+            operatorTree.addNode(operatorTuple[0])  # Create a child node with whatever the actual value is from the input tuple
+            return operatorTree
+        return  False   #The input tuple did not match our method
+
+    def assignmentParse(self, assignmentTuple):
+        if assignmentTuple[1] == "<assignment>":      # If we are dealing with an assignment
+            assignmentTree = parseTree("<assignment>")  # Create a node with the <assignment> value
+            assignmentTree.addNode(assignmentTuple[0])  # Create a child node with whatever the int value is from the input tuple
+            return assignmentTree
+        return  False   #The input tuple did not match our method
+
+    def lookAhead(self, inputList, currentIndex):   #This method looks ahead to the next token to see it's classification
+        return inputList[currentIndex+1][1]
+
+    def Parse(self, input):
         identifier = re.compile("[a-z][a-z]+")
         integer = re.compile("\d")
         assignment = re.compile("<")
-        operator = re.compile()
-        literal = re.compile("\d|[a-z][a-z]+")
-        expr = re.compile("(\d|[a-z][a-z]+)")
+        #literal = re.compile("\d|[a-z][a-z]+")
+        #expr = re.compile("(\d|[a-z][a-z]+)")
 
         if len(input) != 0:
             tree = parseTree("<program>")
-            i = 0
-            while i < len(input):
-                #if re.match(identifier, input[i][0]) & re.match(assignment, input[i+1][0])
+            self.progParse()rogParse()
+
 
 
 lexer.Tokenize("5 HYPERPARATHYROIDisM < 99 COUNTERinTELLIGIENCE the PURPLE Wcontext ofFICIAL POLYmath")
